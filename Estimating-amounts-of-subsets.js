6c208d77e3a7de9889e8964b355909e89c52b1be
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-07-15 18:06:35 -0700

    Update Estimating-amounts-of-subsets.js

--- Estimating-amounts-of-subsets.js
+++ Estimating-amounts-of-subsets.js
@@ -34,3 +34,29 @@ function estSubsets(arr) {
 }
 
 // or
+
+const helper = (a, b) => {
+  let prod = a, i=a;
+  while(i++ < b) {
+    prod *= i;
+  }
+  return prod;
+}
+
+const combinatoric = (n, r) => {
+  if(n == r || r == 0)
+   return 1;
+  r = (r < (n - r)) ? n - r : r;
+  return helper(r + 1, n) / helper(1, n - r)
+}
+
+function estSubsets(arr) {
+    let n = 0;
+    const uniqueArr = Array.from(new Set(arr));
+    let i = 1;
+    while (i <= uniqueArr.length){
+      n += combinatoric(uniqueArr.length, i);
+      i++;
+    }
+    return n // n = amount of subsets that don not have repeated elements
+}
